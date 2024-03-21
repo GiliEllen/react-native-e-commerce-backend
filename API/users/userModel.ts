082acb1ae2ea24e92ebb8d58676eeb1fbb0646b7
gili <gilimena95@gmail.com>
2024-03-21 17:54:11 +0200

    Update userModel.ts

--- API/users/userModel.ts
+++ API/users/userModel.ts
@@ -1,10 +1,18 @@
 import { Schema, model } from 'mongoose'
 
+enum Role {
+    ADMIN = "admin",
+    USER = "user"
+}
+
 const userSchema = new Schema({
     name: { type: String },
     email: { type: String },
     password: { type: String },
+    role: { type: String, default: Role.ADMIN }
 })
+
+
 
 const User = model('users', userSchema)
 
